ed77121bf342690f7b5c4f150a56b9578b309445
Jonathan Allen <grauenwolf@gmail.com>
2020-08-02 20:42:14 -0700

    Convert tests to run under .NET Core

---
+++ TravellerTools/Grauenwolf.TravellerTools.Tests/AnimalEncounters.cs
@@ -0,0 +1,65 @@
+﻿using CSScriptLib;
+using Grauenwolf.TravellerTools.Animals.AE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grauenwolf.TravellerTools.Animals.Tests
+{
+    [TestClass]
+    public class AnimalEncounters
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Dice dice = new Dice();
+            var a = new Animal();
+            a.Armor = 5;
+            AnimalBuilderAE.RunScript(a, dice, "animal.Armor = 0;");
+            Assert.AreEqual(0, a.Armor);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Dice dice = new Dice();
+            var animal = new Animal();
+            animal.Skills.Add("Stealth", 2);
+            animal.Skills.Remove("Stealth");
+            AnimalBuilderAE.RunScript(animal, dice, "animal.Skills.Remove(\"Stealth\");");
+            Assert.IsNull(animal.Skills["Stealth"]);
+
+            if (animal.Movement == "Flight")
+            {
+                animal.Movement = "Walk";
+                animal.Strength += dice.D(1, 6);
+            }
+            else
+            {
+                animal.Movement += " Flight";
+                animal.Strength -= 1;
+                animal.Armor -= 1;
+            }
+        }
+
+        [TestMethod]
+        public void TestCSSCript()
+        {
+            dynamic calc = CSScript.Evaluator
+                       .LoadMethod(@"int add(int a, int b)
+                                        {
+                                            return a + b;
+                                        }");
+            var result = calc.add(1, 3);
+        }
+
+        //Non-deterministic test. Only run locally.
+        //[TestMethod]
+        //public void TestMethod3()
+        //{
+        //    AnimalBuilderAE.SetDataPath(".");
+        //    Parallel.For(0, 1000, _ =>
+        //    {
+        //        var result = AnimalBuilderAE.BuildPlanetSet();
+        //    });
+        //}
+    }
+}
