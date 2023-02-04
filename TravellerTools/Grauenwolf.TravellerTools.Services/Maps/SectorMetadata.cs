619cfba3ade6750798cdcd39d64618984854d880
Jonathan Allen <grauenwolf@gmail.com>
2023-02-03 16:55:05 -0800

    Updated to match new JSON format.

--- TravellerTools/Grauenwolf.TravellerTools.Services/Maps/SectorMetadata.cs
+++ TravellerTools/Grauenwolf.TravellerTools.Services/Maps/SectorMetadata.cs
@@ -7,7 +7,7 @@ namespace Grauenwolf.TravellerTools.Maps
 		public string Tags { get; set; }
 		public string Abbreviation { get; set; }
 		public Name[] Names { get; set; }
-		public string Credits { get; set; }
+		public string[] Credits { get; set; }
 		public Datafile DataFile { get; set; }
 		public int X { get; set; }
 		public int Y { get; set; }
